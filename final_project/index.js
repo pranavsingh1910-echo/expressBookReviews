faf7f63eada9b8f3473c2e9f5f145b4d727a3b08
pranavsingh1910-echo <pranavsingh1910@gmail.com>
2026-09-24 20:40:53 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,25 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    if (!req.session.authorization) {
+        return res.status(401).json({
+            message: "User not logged in"
+        });
+    }
+
+    const token = req.session.authorization.accessToken;
+
+    jwt.verify(token, "access", function(err, decoded) {
+
+        if (err) {
+            return res.status(403).json({
+                message: "Invalid or expired token"
+            });
+        }
+
+        req.user = decoded.username;
+        next();
 });
  
 const PORT =5000;
